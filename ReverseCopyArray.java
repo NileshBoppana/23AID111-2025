e5f3fef1d6d2cfb25060d1ccbf8d2c41e0504b9f
NileshBoppana <nileshboppana2008@gmail.com>
2026-01-07 14:13:29 +0530

    Create ReverseCopyArray.java

---
+++ ReverseCopyArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ReverseCopyArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        System.out.println("Enter elements of the first array:");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        // Copy in reverse order
+        for (int i = 0; i < n; i++) {
+            arr2[i] = arr1[n - 1 - i];
+        }
+
+        System.out.println("Second array (reverse order):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+        sc.close();
+    }
+}
