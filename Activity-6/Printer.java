913c49e0d8f4008881111f1ab033f97f498ce6b3
NileshBoppana <nileshboppana2008@gmail.com>
2026-03-15 14:27:30 +0530

    Update Printer.java

--- Activity-6/Printer.java
+++ Activity-6/Printer.java
@@ -1,22 +1,43 @@
+import java.util.Scanner;
+class Printers{
+	void print(int num) {
+		System.out.println(num);
+	}
+	void print(String xyz) {
+		System.out.println(xyz);
+	}
+	void print(int n, String abc) {
+		System.out.println("number is " + n + "\nstring is " + abc);
+	}
+}
 public class Printer {
 
-    void print(int n) {
-        System.out.println("Number: " + n);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number - ");
+		int number = sc.nextInt();
+		System.out.print("Enter the string - ");
+		String text = sc.next();
+		
+		int index;
+		Printers obj = new Printers();
+		System.out.println("Enter your choice what do you want to print \n1 - number\n2 - string\n3 - both number and string");
+		index = sc.nextInt();
+		
+		switch(index) {
+		case 1 : 
+			obj.print(number);
+			break;
+		case 2 :
+			obj.print(text);
+			break;
+		case 3 : 
+			obj.print(index, text);
+			break;
+		default : 
+			System.out.println("Index not found");
+		}
+		sc.close();
+	}
 
-    void print(String t) {
-        System.out.println("Text: " + t);
-    }
-
-    void print(int n, String t) {
-        System.out.println("Number: " + n + " Text: " + t);
-    }
-
-    public static void main(String[] args) {
-        Printer p = new Printer();
-
-        p.print(10);
-        p.print("Hello");
-        p.print(5, "Pages");
-    }
 }
