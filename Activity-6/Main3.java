465768b8eb0b95a8bf1ab080610a1eaf5302ab6a
NileshBoppana <nileshboppana2008@gmail.com>
2026-03-19 22:07:20 +0530

    Activity-6

--- Activity-6/Main3.java
+++ Activity-6/Main3.java
@@ -1,42 +1,32 @@
 import java.util.Scanner;
 
 class Logger {
-
     void log(String message) {
         System.out.println("Message: " + message);
     }
-
     void log(String message, String time) {
         System.out.println("Message: " + message + " Time: " + time);
     }
-
     void log(String message, String time, int code) {
         System.out.println("Message: " + message + " Time: " + time + " ErrorCode: " + code);
     }
 }
-
 public class Main3 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         Logger l = new Logger();
-
         System.out.println("1 Message");
         System.out.println("2 Message with Time");
         System.out.println("3 Message with Time and ErrorCode");
         System.out.print("Enter choice: ");
-
         int choice = sc.nextInt();
         sc.nextLine();
-
         switch (choice) {
-
             case 1:
                 System.out.print("Enter message: ");
                 String m1 = sc.nextLine();
                 l.log(m1);
                 break;
-
             case 2:
                 System.out.print("Enter message: ");
                 String m2 = sc.nextLine();
@@ -44,7 +34,6 @@ public class Main3 {
                 String t = sc.nextLine();
                 l.log(m2, t);
                 break;
-
             case 3:
                 System.out.print("Enter message: ");
                 String m3 = sc.nextLine();
@@ -54,7 +43,6 @@ public class Main3 {
                 int code = sc.nextInt();
                 l.log(m3, t1, code);
                 break;
-
             default:
                 System.out.println("Invalid choice");
         }
