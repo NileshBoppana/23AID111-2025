8618d631edb015458ccadd3c991d1520bd0f545f
NileshBoppana <nileshboppana2008@gmail.com>
2026-03-25 14:48:34 +0530

    Activity-5

--- Activity-5/Main54.java
+++ Activity-5/Main54.java
@@ -21,6 +21,7 @@ public class Main54 {
     public static void main(String[] args) {
 
         Employee emp1 = new Employee("Alice");
+        Employee emp2 = new Employee("Bob", 50000);
 
         emp1.display();
         emp2.display();
